180bbe71fd80b6a3677145cdf36806c8f00a5c05
Solomon Montagno <sjmontagno@gmail.com>
2023-08-22 15:57:34 -0400

    Finished product

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,15 +1,11 @@
 import { Component } from "react";
+import "./card-list.styles.css";
+import CardContainer from "../card-container/card-container.component";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
-    return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
-      </div>
-    );
+    return <CardContainer monsters={monsters} />;
   }
 }
 
